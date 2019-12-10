519e49991270cee2f41b2f3ed5a9f49dc665e36a
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-10 16:41:26 +0700

    Implement Register

--- app.py
+++ app.py
@@ -5,21 +5,17 @@ from flask import jsonify
 from flask import Flask
 
 app = Flask(__name__)
-
+client = pymongo.MongoClient("mongodb+srv://airpeoject:cYhdo8-kacnuc-wubtak@cluster0-rnpzg.gcp.mongodb.net/")
+db = client.air_quality
 
 @app.route("/")
 def check():
     return 'API is running..'
 
-
 @app.route("/register")
 def register():
-    client = pymongo.MongoClient(
-        "mongodb+srv://airpeoject:cYhdo8-kacnuc-wubtak@cluster0-rnpzg.gcp.mongodb.net/")
-    db = client.air_quality
     usr = request.args.get('username')
     pwd = request.args.get('password')
     form = {"username": usr, "password": pwd}
     db.data.insert_one(form)
     return 'Success'
-    
