f56c0d1042586544b37c9dfce304661833cf2629
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-10 17:11:53 +0700

    Remove All

--- app.py
+++ app.py
@@ -5,17 +5,7 @@ from flask import jsonify
 from flask import Flask
 
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb+srv://airpeoject:cYhdo8-kacnuc-wubtak@cluster0-rnpzg.gcp.mongodb.net/")
-db = client.air_quality
 
 @app.route("/")
 def check():
     return 'API is running..'
-
-@app.route("/register")
-def register():
-    usr = request.args.get('username')
-    pwd = request.args.get('password')
-    form = {"username": usr, "password": pwd}
-    db.data.insert_one(form)
-    return 'Success'
