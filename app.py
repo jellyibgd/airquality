5695d9f32fb61cbece9903f95f7e4c6d4701789f
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-10 16:22:31 +0700

    Add feature

--- app.py
+++ app.py
@@ -22,39 +22,4 @@ def register():
     form = {"username": usr, "password": pwd}
     db.data.insert_one(form)
     return 'Success'
-
-
-@app.route("/addplaces")
-def addplaces():
-    client = pymongo.MongoClient(
-        "mongodb+srv://airpeoject:cYhdo8-kacnuc-wubtak@cluster0-rnpzg.gcp.mongodb.net/")
-    db = client.air_quality
-    usr = request.args.get('username')
-    station = request.args.get('station')
-    db.data.update(
-     { "username": usr },
-     { $set: { "places": station } }
-    )
-    return 'Success'
-
-
-@app.route("/search")
-def search():
-    query = request.args.get('q')
-    r = requests.get("https://website-api.airvisual.com/v1/search?q="+query +
-                     "&units.temperature=celsius&units.distance=kilometer&AQI=US&language=en")
-    data = r.json()
-    return data
-
-
-@app.route("/fetch")
-def fetch():
-    query = request.args.get('q')
-    r = requests.get("https://website-api.airvisual.com/v1/stations/"+station)
-    data = r.json()
-    return data
-
-@app.route("/userfetch")
-def userfetch():
-    username = request.args.get('usr')
-    return 'Success'
+    
